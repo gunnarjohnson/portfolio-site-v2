a52a741c99ffd335aaa36101b60d49b56232ed18
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2018-08-14 13:17:08 -0500

    Add elements to hero and modify styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons/faGithubSquare';
 
 const Hero = (props) => (
   <section className={"hero section section--theme-" + props.theme}>
     <h1 className="section__title hero__title">Gunnar Johnson</h1>
     <p className={"hero__content hero__content--theme-" + props.theme}>Web Developer</p>
+    <div className="hero__icon-container">
+      <a className="hero__icon" href="https://www.facebook.com/gunnarjohnson" target="_blank">
+        <FontAwesomeIcon icon={faFacebookSquare} />
+      </a>
+      <a className="hero__icon" href="https://github.com/gunnarjohnson" target="_blank">
+        <FontAwesomeIcon icon={faGithubSquare} />
+      </a>
+      <a className="hero__icon" href="https://www.linkedin.com/in/gunnarmjohnson" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+    </div>
   </section>
 );
 
