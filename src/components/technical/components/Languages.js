6714f78437cdf4987a4725d53991a99b99762b7c
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2024-03-09 14:21:45 -0600

    Update Technical Skills section

--- src/components/technical/components/Languages.js
+++ src/components/technical/components/Languages.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Languages = () => (
 	<div className="technical-subset">
 		<h3 className="heading technical-subset__title section__subtitle">Languages</h3>
-		<p className="content section__content">CSS3, HTML5, JavaScript (ES10), PHP, Python, Sass, SQL</p>
+		<p className="content section__content">CSS, HTML, JavaScript, PHP, Python, Sass, SQL</p>
 	</div>
-)
+);
 
 export default Languages;
