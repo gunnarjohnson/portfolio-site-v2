3b761c796b61de0fbab8a927a07e74a612130190
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2024-03-13 17:26:14 -0500

    Update Hero and About sections - Update content - Refactor code

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,22 @@
 import React from 'react';
 
-const About = (props) => (
-  <section id="about" className={"about section section--theme-" + props.theme}>
-    <h2 className="heading about__title section__title">About</h2>
-    <p className="about__content section__content">
-      <span className="about__content-block">
-        <span className="content">
-          <span className="section__content--uppercase">I build web apps and websites. </span>
-          The Internet is always evolving and I love learning its new technologies. 
-          Every day affords fresh opportunities to innovate. 
-          In addition to developing websites for clients, I enjoy working on personal projects, such as my
-        </span>
-        {' '}
-        <a 
-          className="link about__content-link" 
-          href="https://github.com/gunnarjohnson/react-boilerplate" 
-          target="_blank"
-        >
-          React Boilerplate
-        </a> 
-        {' '}
-        <span className="content">
-          - the foundation of this site.
-        </span>
-      </span>
-      <span className="content about__content-block">
-        When I'm not developing, I spend my time brewing beer, playing guitar, reading science fiction, or researching my latest esoteric interest.
-        I also love to travel with my wife, and we visit the PNW at least once a year to seek out scenic hikes (and artisanal donuts).
-      </span>
-      <span className="content about__content-block">
-        Thanks for stopping by - check out some of my work below!
-      </span>
-    </p>
-  </section>
+const About = ({ theme }) => (
+	<section id="about" className={`about section section--theme-${theme}`}>
+		<h2 className="heading about__title section__title">About</h2>
+		<p className="about__content section__content">
+			<span className="content about__content-block">
+				<span className="section__content--uppercase">I build web applications and websites. </span>
+				The Internet is always evolving and I love learning its new technologies. Every day affords fresh opportunities
+				to innovate.
+			</span>
+			<span className="content about__content-block">
+				When I'm not developing, you might find me brewing a farmhouse ale, building a guitar pedal, or reading a good
+				book. I also love to travel with my family, and we visit the PNW at least once a year to seek out scenic hikes
+				(and Portland donuts).
+			</span>
+			<span className="content about__content-block">Thanks for stopping by, and check out some of my work below!</span>
+		</p>
+	</section>
 );
 
 export default About;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons/faGithubSquare';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Hero = (props) => (
-  <section className={"hero section section--theme-" + props.theme}>
-    <h1 className="heading section__title hero__title">Gunnar Johnson</h1>
-    <p className={"content hero__content hero__content--theme-" + props.theme}>Web Developer</p>
-    <div className="hero__icon-container">
-      <a className="link hero__icon" href="https://www.facebook.com/gunnarjohnson" target="_blank">
-        <FontAwesomeIcon icon={faFacebookSquare} />
-      </a>
-      <a className="link hero__icon" href="https://github.com/gunnarjohnson" target="_blank">
-        <FontAwesomeIcon icon={faGithubSquare} />
-      </a>
-      <a className="link hero__icon" href="https://www.linkedin.com/in/gunnarmjohnson" target="_blank">
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
-    </div>
-  </section>
+const Hero = ({ theme }) => (
+	<section className={`hero section section--theme-${theme}`}>
+		<h1 className="heading section__title hero__title">Gunnar Johnson</h1>
+		<p className={`content hero__content hero__content--theme-${theme}`}>Software Engineer</p>
+		<div className="hero__icon-container">
+			<a className="link hero__icon" href="https://www.facebook.com/gunnarjohnson" target="_blank">
+				<FontAwesomeIcon icon={faFacebookSquare} />
+			</a>
+			<a className="link hero__icon" href="https://github.com/gunnarjohnson" target="_blank">
+				<FontAwesomeIcon icon={faGithubSquare} />
+			</a>
+			<a className="link hero__icon" href="https://www.linkedin.com/in/gunnarmjohnson" target="_blank">
+				<FontAwesomeIcon icon={faLinkedin} />
+			</a>
+		</div>
+	</section>
 );
 
 export default Hero;
