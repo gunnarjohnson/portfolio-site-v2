797f7047fa156ac8c5860399e56039ad73044372
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-01-31 10:29:46 -0600

    Update dependencies - Update package.json   - Rename main from index.js to app.js   - Bump Babel dependencies   - Change favicons from 7.2.0 to ^7.2.0   - Change favicons-webpack-plugin from 6.0.1 to ^6.0.1   - Remove postcss-strip-inline-comments - Update PostCSS config   - Remove postcss-strip-inline-comments plugin - Update lockfile - Add .nvmrc

--- postcss.config.js
+++ postcss.config.js
@@ -3,16 +3,8 @@ const postcssMediaMinmax = require('postcss-media-minmax');
 const postcssNested = require('postcss-nested');
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssSimpleVars = require('postcss-simple-vars');
-const postcssStripInlineComments = require('postcss-strip-inline-comments');
 
 module.exports = {
   syntax: 'postcss-scss',
-  plugins: [
-    postcssImport,
-    postcssSimpleVars,
-    postcssMediaMinmax,
-    postcssNested,
-    postcssStripInlineComments,
-    postcssPresetEnv,
-  ],
+  plugins: [postcssImport, postcssSimpleVars, postcssMediaMinmax, postcssNested, postcssPresetEnv],
 };
