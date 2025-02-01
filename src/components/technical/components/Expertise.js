3928f1d713dd4b3dca437460917864ad4f7426de
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-01-31 22:07:08 -0600

    Refactor Expertise component

--- src/components/technical/components/Expertise.js
+++ src/components/technical/components/Expertise.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
+const listItems = [
+  'Front-End Architecture',
+  'Responsive Web Design',
+  'Semantic Markup',
+  'UX Principles',
+  'Version Control',
+];
+
 const Expertise = () => (
   <div className="technical-subset">
     <h3 className="heading technical-subset__title section__subtitle">Expertise</h3>
     <ul className="list technical-subset__list section__content">
-      <li className="technical-subset__list-item">Front-End Architecture</li>
-      <li className="technical-subset__list-item">Responsive Web Design</li>
-      <li className="technical-subset__list-item">Semantic Markup</li>
-      <li className="technical-subset__list-item">UX Principles</li>
-      <li className="technical-subset__list-item">Version Control</li>
+      {listItems.map((listItem) => (
+        <li
+          key={listItem}
+          className="technical-subset__list-item"
+        >
+          {listItem}
+        </li>
+      ))}
     </ul>
   </div>
 );
