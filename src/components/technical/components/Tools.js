3eb757a81caae5eeec81aacc2071fc767163f15d
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-01-31 22:03:46 -0600

    Refactor Tools component

--- src/components/technical/components/Tools.js
+++ src/components/technical/components/Tools.js
@@ -1,54 +1,57 @@
 import React from 'react';
 
+const listItems = [
+  {
+    listItem: 'CMS',
+    subListItems: ['Netlify CMS', 'Payload CMS', 'WordPress'],
+  },
+  {
+    listItem: 'FWKs',
+    subListItems: ['Vue', 'Remix', 'Gatsby', 'PostCSS', 'Tailwind CSS', 'Bootstrap', 'Laravel'],
+  },
+  {
+    listItem: 'Libs',
+    subListItems: ['React', 'React Native', 'TanStack Query', 'TanStack Router', 'jQuery'],
+  },
+  {
+    listItem: 'Misc',
+    subListItems: [
+      'React Testing Library',
+      'Jest',
+      'Vite',
+      'Webpack',
+      'Sass',
+      'Storybook',
+      'Git',
+      'PostgreSQL',
+      'MySQL',
+      'Figma',
+    ],
+  },
+];
+
 const Tools = () => (
   <div className="technical-subset">
     <h3 className="heading technical-subset__title section__subtitle">Tools</h3>
     <ul className="list technical-subset__list section__content">
-      <li className="technical-subset__list-item">
-        CMS
-        <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Netlify CMS</li>
-          <li className="technical-subset__sub-list-item">Payload CMS</li>
-          <li className="technical-subset__sub-list-item">WordPress</li>
-        </ul>
-      </li>
-      <li className="technical-subset__list-item">
-        FWKs
-        <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Vue</li>
-          <li className="technical-subset__sub-list-item">Remix</li>
-          <li className="technical-subset__sub-list-item">Gatsby</li>
-          <li className="technical-subset__sub-list-item">PostCSS</li>
-          <li className="technical-subset__sub-list-item">Tailwind CSS</li>
-          <li className="technical-subset__sub-list-item">Bootstrap</li>
-          <li className="technical-subset__sub-list-item">Laravel</li>
-        </ul>
-      </li>
-      <li className="technical-subset__list-item">
-        Libs
-        <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">React</li>
-          <li className="technical-subset__sub-list-item">React Native</li>
-          <li className="technical-subset__sub-list-item">TanStack Query</li>
-          <li className="technical-subset__sub-list-item">TanStack Router</li>
-          <li className="technical-subset__sub-list-item">jQuery</li>
-        </ul>
-      </li>
-      <li className="technical-subset__list-item">
-        Misc
-        <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">React Testing Library</li>
-          <li className="technical-subset__sub-list-item">Jest</li>
-          <li className="technical-subset__sub-list-item">Vite</li>
-          <li className="technical-subset__sub-list-item">Webpack</li>
-          <li className="technical-subset__sub-list-item">Sass</li>
-          <li className="technical-subset__sub-list-item">Storybook</li>
-          <li className="technical-subset__sub-list-item">Git</li>
-          <li className="technical-subset__sub-list-item">PostgreSQL</li>
-          <li className="technical-subset__sub-list-item">MySQL</li>
-          <li className="technical-subset__sub-list-item">Figma</li>
-        </ul>
-      </li>
+      {listItems.map(({ listItem, subListItems }) => (
+        <li
+          key={listItem}
+          className="technical-subset__list-item"
+        >
+          {listItem}
+          <ul className="technical-subset__sub-list">
+            {subListItems.map((subListItem) => (
+              <li
+                key={subListItem}
+                className="technical-subset__sub-list-item"
+              >
+                {subListItem}
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
     </ul>
   </div>
 );
