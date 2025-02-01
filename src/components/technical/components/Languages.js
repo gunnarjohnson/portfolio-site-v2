fc95ae7fe3f12135c75ddc5e1cc5086a446945b7
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-01-31 20:45:19 -0600

    Update Technical Skills section

--- src/components/technical/components/Languages.js
+++ src/components/technical/components/Languages.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+const languagesList = [
+  'JavaScript',
+  'TypeScript',
+  'HTML',
+  'CSS',
+  'Markdown',
+  'Node.js',
+  'SQL',
+  'Python',
+  'PHP',
+];
+
 const Languages = () => (
   <div className="technical-subset">
     <h3 className="heading technical-subset__title section__subtitle">Languages</h3>
-    <p className="content section__content">CSS, HTML, JavaScript, PHP, Python, Sass, SQL</p>
+    <ul className="list technical-subset__list content section__content">
+      {languagesList.map((language) => (
+        <li
+          key={language}
+          className="technical-subset__list-item technical-subset__list-item--inline"
+        >
+          {language}
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
--- src/components/technical/components/Tools.js
+++ src/components/technical/components/Tools.js
@@ -7,42 +7,46 @@ const Tools = () => (
       <li className="technical-subset__list-item">
         CMS
         <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Netlify CMS, WordPress</li>
+          <li className="technical-subset__sub-list-item">Netlify CMS</li>
+          <li className="technical-subset__sub-list-item">Payload CMS</li>
+          <li className="technical-subset__sub-list-item">WordPress</li>
         </ul>
       </li>
       <li className="technical-subset__list-item">
         FWKs
         <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Bootstrap</li>,
-          <li className="technical-subset__sub-list-item">Laravel</li>,
-          <li className="technical-subset__sub-list-item">PostCSS</li>,
           <li className="technical-subset__sub-list-item">Vue</li>
-        </ul>
-      </li>
-      <li className="technical-subset__list-item">
-        Git
-        <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Bash</li>,
-          <li className="technical-subset__sub-list-item">CMD</li>,
-          <li className="technical-subset__sub-list-item">GitHub</li>,
-          <li className="technical-subset__sub-list-item">GitLab</li>
+          <li className="technical-subset__sub-list-item">Remix</li>
+          <li className="technical-subset__sub-list-item">Gatsby</li>
+          <li className="technical-subset__sub-list-item">PostCSS</li>
+          <li className="technical-subset__sub-list-item">Tailwind CSS</li>
+          <li className="technical-subset__sub-list-item">Bootstrap</li>
+          <li className="technical-subset__sub-list-item">Laravel</li>
         </ul>
       </li>
       <li className="technical-subset__list-item">
         Libs
         <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">jQuery</li>,
           <li className="technical-subset__sub-list-item">React</li>
+          <li className="technical-subset__sub-list-item">React Native</li>
+          <li className="technical-subset__sub-list-item">TanStack Query</li>
+          <li className="technical-subset__sub-list-item">TanStack Router</li>
+          <li className="technical-subset__sub-list-item">jQuery</li>
         </ul>
       </li>
       <li className="technical-subset__list-item">
         Misc
         <ul className="technical-subset__sub-list">
-          <li className="technical-subset__sub-list-item">Babel</li>,
-          <li className="technical-subset__sub-list-item">Gulp</li>,
-          <li className="technical-subset__sub-list-item">NPM</li>,
-          <li className="technical-subset__sub-list-item">phpMyAdmin</li>,
+          <li className="technical-subset__sub-list-item">React Testing Library</li>
+          <li className="technical-subset__sub-list-item">Jest</li>
+          <li className="technical-subset__sub-list-item">Vite</li>
           <li className="technical-subset__sub-list-item">Webpack</li>
+          <li className="technical-subset__sub-list-item">Sass</li>
+          <li className="technical-subset__sub-list-item">Storybook</li>
+          <li className="technical-subset__sub-list-item">Git</li>
+          <li className="technical-subset__sub-list-item">PostgreSQL</li>
+          <li className="technical-subset__sub-list-item">MySQL</li>
+          <li className="technical-subset__sub-list-item">Figma</li>
         </ul>
       </li>
     </ul>
