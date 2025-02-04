ba463d6eb97128ed89a75a784ebca6e3762251a8
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-04 17:07:14 -0600

    Refactor Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,38 +5,41 @@ import { faGithubSquare } from '@fortawesome/free-brands-svg-icons/faGithubSquar
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const heroIcons = [
+  {
+    ariaLabel: 'Visit Facebook page',
+    icon: faFacebookSquare,
+    url: 'https://www.facebook.com/gunnarjohnson',
+  },
+  {
+    ariaLabel: 'Visit GitHub page',
+    icon: faGithubSquare,
+    url: 'https://github.com/gunnarjohnson',
+  },
+  {
+    ariaLabel: 'Visit LinkedIn page',
+    icon: faLinkedin,
+    url: 'https://www.linkedin.com/in/gunnarmjohnson',
+  },
+];
+
 const Hero = ({ theme }) => (
   <section className={`hero section section--theme-${theme}`}>
     <h1 className="heading section__title hero__title">Gunnar Johnson</h1>
     <p className={`content hero__content hero__content--theme-${theme}`}>Software Engineer</p>
     <div className="hero__icon-container">
-      <a
-        aria-label="Visit Facebook page"
-        className="link hero__icon"
-        href="https://www.facebook.com/gunnarjohnson"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faFacebookSquare} />
-      </a>
-      <a
-        aria-label="Visit GitHub page"
-        className="link hero__icon"
-        href="https://github.com/gunnarjohnson"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faGithubSquare} />
-      </a>
-      <a
-        aria-label="Visit LinkedIn page"
-        className="link hero__icon"
-        href="https://www.linkedin.com/in/gunnarmjohnson"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
+      {heroIcons.map(({ ariaLabel, icon, url }) => (
+        <a
+          key={url}
+          aria-label={ariaLabel}
+          className="link hero__icon"
+          href={url}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={icon} />
+        </a>
+      ))}
     </div>
   </section>
 );
