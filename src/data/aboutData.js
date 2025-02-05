a1653994c13b7b744f3e9b489a9eefa55c959701
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 10:12:55 -0600

    Refactor About component - Move static data to aboutData module - Refactor component

---
+++ src/data/aboutData.js
@@ -0,0 +1,21 @@
+const aboutData = [
+  {
+    id: 1,
+    introduction: 'I build web apps, mobile apps, and websites.',
+    content:
+      'The Internet is always evolving and I love learning its new technologies. Every day affords fresh opportunities to innovate.',
+  },
+  {
+    id: 2,
+    introduction: null,
+    content:
+      "When I'm not developing, you might find me brewing a farmhouse ale, building a guitar pedal, or reading a good book. I also love to travel with my family, and we visit the PNW every year to seek out scenic hikes (and Portland donuts).",
+  },
+  {
+    id: 3,
+    introduction: null,
+    content: 'Thanks for stopping by, and check out some of my work below!',
+  },
+];
+
+export default aboutData;
