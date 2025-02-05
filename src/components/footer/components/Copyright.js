f28b9d45492ca2ffae2935793479a8c8097cf626
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 11:48:09 -0600

    Refactor Copyright component

--- src/components/footer/components/Copyright.js
+++ src/components/footer/components/Copyright.js
@@ -2,50 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import footerData from '../../../data/footerData';
 
-const { copyrightName, copyrightUrl, copyrightYear: defaultCopyrightYear } = footerData;
+const { copyrightName, copyrightUrl, copyrightYear } = footerData;
 
-class Copyright extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      copyrightYear: defaultCopyrightYear,
-      currentYear: undefined,
-    };
-  }
+const Copyright = ({ theme }) => {
+  const currentYear = new Date().getFullYear();
+  const shouldDisplayCurrentYear = currentYear !== copyrightYear;
 
-  componentDidMount() {
-    const { copyrightYear } = this.state;
-    const currentYear = new Date().getFullYear();
-
-    if (currentYear !== copyrightYear) {
-      this.setState({ currentYear });
-    }
-  }
-
-  render() {
-    const { copyrightYear, currentYear } = this.state;
-    const { theme } = this.props;
-
-    return (
-      <div className="copyright">
-        <p className="copyright__content section__content">
-          <span className="content">
-            © {copyrightYear}
-            {currentYear && `–${currentYear}`}{' '}
-          </span>
-          <a
-            className={`link copyright__link copyright__link--theme-${theme}`}
-            href={copyrightUrl}
-            rel="noreferrer"
-            target="_blank"
-          >
-            {copyrightName}
-          </a>
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="copyright">
+      <p className="copyright__content section__content">
+        <span className="content">
+          © {copyrightYear}
+          {shouldDisplayCurrentYear && `–${currentYear}`}{' '}
+        </span>
+        <a
+          className={`link copyright__link copyright__link--theme-${theme}`}
+          href={copyrightUrl}
+          rel="noreferrer"
+          target="_blank"
+        >
+          {copyrightName}
+        </a>
+      </p>
+    </div>
+  );
+};
 
 Copyright.propTypes = {
   theme: PropTypes.string.isRequired,
