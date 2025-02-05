81fec5b5f01ecc28edfdaf3f0359d0a807e97b17
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 10:31:40 -0600

    Refactor Hero component - Move static data to heroData module - Refactor component

---
+++ src/data/heroData.js
@@ -0,0 +1,23 @@
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons/faFacebookSquare';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons/faGithubSquare';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+
+const heroIcons = [
+  {
+    ariaLabel: 'Visit Facebook page',
+    icon: faFacebookSquare,
+    url: 'https://www.facebook.com/gunnarjohnson',
+  },
+  {
+    ariaLabel: 'Visit GitHub page',
+    icon: faGithubSquare,
+    url: 'https://github.com/gunnarjohnson',
+  },
+  {
+    ariaLabel: 'Visit LinkedIn page',
+    icon: faLinkedin,
+    url: 'https://www.linkedin.com/in/gunnarmjohnson',
+  },
+];
+
+export default { heroIcons };
