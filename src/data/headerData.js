ec81343550b7234aee1dbba2a6de47e847d3d0fc
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 11:07:53 -0600

    Refactor DesktopNav component - Update headerData - Refactor component

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,21 +1,25 @@
 const navOptions = [
   {
     key: 'about',
+    text: 'About',
     url: '#about',
     icon: 'user',
   },
   {
     key: 'portfolio',
+    text: 'Portfolio',
     url: '#portfolio',
     icon: 'clipboard-list',
   },
   {
     key: 'technical',
+    text: 'Skills',
     url: '#technical',
     icon: 'laptop-code',
   },
   {
     key: 'contact',
+    text: 'Contact',
     url: '#contact',
     icon: 'envelope',
   },
