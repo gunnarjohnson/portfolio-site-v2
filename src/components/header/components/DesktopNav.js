057398cd4c9115a87bf7ba0a1ebc4365b7ca3b00
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 11:07:53 -0600

    Refactor DesktopNav component - Update headerData - Refactor component

--- src/components/header/components/DesktopNav.js
+++ src/components/header/components/DesktopNav.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import headerData from '../../../data/headerData';
+
+const { navOptions } = headerData;
 
 const DesktopNav = () => (
   <nav className="header__desktop-nav">
-    <AnchorLink
-      className="link header__desktop-nav-link section__content"
-      href="#about"
-      offset="70"
-    >
-      About
-    </AnchorLink>
-    <AnchorLink
-      className="link header__desktop-nav-link section__content"
-      href="#portfolio"
-      offset="70"
-    >
-      Portfolio
-    </AnchorLink>
-    <AnchorLink
-      className="link header__desktop-nav-link section__content"
-      href="#technical"
-      offset="70"
-    >
-      Skills
-    </AnchorLink>
-    <AnchorLink
-      className="link header__desktop-nav-link section__content"
-      href="#contact"
-      offset="70"
-    >
-      Contact
-    </AnchorLink>
+    {navOptions.map(({ key, url, text }) => (
+      <AnchorLink
+        key={key}
+        className="link header__desktop-nav-link section__content"
+        href={url}
+        offset="70"
+      >
+        {text}
+      </AnchorLink>
+    ))}
   </nav>
 );
 
