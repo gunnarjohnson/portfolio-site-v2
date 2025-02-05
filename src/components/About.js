46cb2f4ed56a34797d5277910a72ae0bdc3d785b
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-02-05 10:12:55 -0600

    Refactor About component - Move static data to aboutData module - Refactor component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import aboutData from '../data/aboutData';
 
 const About = ({ theme }) => (
   <section
@@ -8,21 +9,15 @@ const About = ({ theme }) => (
   >
     <h2 className="heading about__title section__title">About</h2>
     <p className="about__content section__content">
-      <span className="content about__content-block">
-        <span className="section__content--uppercase">
-          I build web apps, mobile apps, and websites.{' '}
+      {aboutData.map(({ content, id, introduction }) => (
+        <span
+          key={id}
+          className="content about__content-block"
+        >
+          {introduction && <span className="section__content--uppercase">{introduction} </span>}
+          {content}
         </span>
-        The Internet is always evolving and I love learning its new technologies. Every day affords
-        fresh opportunities to innovate.
-      </span>
-      <span className="content about__content-block">
-        When I&apos;m not developing, you might find me brewing a farmhouse ale, building a guitar
-        pedal, or reading a good book. I also love to travel with my family, and we visit the PNW
-        every year to seek out scenic hikes (and Portland donuts).
-      </span>
-      <span className="content about__content-block">
-        Thanks for stopping by, and check out some of my work below!
-      </span>
+      ))}
     </p>
   </section>
 );
