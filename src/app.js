d120ec0a00308a29d1395dd8b5440655a899592f
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2025-04-11 13:31:12 -0500

    Wrap PortfolioApp component with ThemeProvider

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import PortfolioApp from './components/PortfolioApp';
+import ThemeProvider from './providers/ThemeProvider';
 import './scripts/faLib';
 import './styles/styles.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-root.render(<PortfolioApp />);
+root.render(
+  <ThemeProvider>
+    <PortfolioApp />
+  </ThemeProvider>,
+);
